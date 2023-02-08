fc2c8e79c13570c86351cbaa1a4e44940de97be8
Daniel Steuri <daniel.steuri@flowingones.ch>
2023-02-08 10:30:26 +0000

    make text bold

--- pages/page.tsx
+++ pages/page.tsx
@@ -4,7 +4,7 @@ import Counter from "app/Counter$.tsx";
 export default () => {
   return (
     <div>
-      <h1 class="text-4xl">Hello World!</h1>
+      <h1 class="text-4xl font-bold">Hello World!</h1>
       <Counter />
     </div>
   );
